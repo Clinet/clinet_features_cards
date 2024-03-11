2f83bd51a11632f2ea7e683634c47d4f4a97dafe
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2024-03-10 23:14:10 -0400

    cards: Initial commit

---
+++ commands.go
@@ -0,0 +1,24 @@
+package cards
+
+import (
+	"math/rand"
+	"strings"
+
+	"github.com/Clinet/clinet_cmds"
+	"github.com/Clinet/clinet_features"
+)
+
+var PLACEHOLDER = "I'm sorry, the ~~-V-O-I-D-~~ is unable to respond."
+var PLACEHOLDER_SMALL = "~~-V-O-I-D-~~"
+
+var Feature = features.Feature{
+	Name: "cards",
+	Desc: "Invites your economy server to participate in trading custom cards.",
+	Cmds: []*cmds.Cmd{
+		cmds.NewCmd("cards", "This doesn't do anything... (yet)", handleCards),
+	},
+}
+
+func handleCards(ctx *cmds.CmdCtx) *cmds.CmdResp {
+	return cmds.NewCmdRespEmbed("Cards", PLACEHOLDER)
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/Clinet/clinet_features_cards
+
+go 1.22.0
+
+require (
+	github.com/Clinet/clinet_cmds v0.0.0-20221210012841-dadccfd56f19
+	github.com/Clinet/clinet_features v0.0.0-20221210013248-ee30597ec785
+)
+
+require (
+	github.com/Clinet/clinet_convos v0.0.0-20221115092451-a56298b8a270 // indirect
+	github.com/Clinet/clinet_services v0.0.0-20221115093202-3b5bd50f66ca // indirect
+	github.com/JoshuaDoes/go-wolfram v0.0.0-20181104014914-c99b0fd7d862 // indirect
+	github.com/JoshuaDoes/json v0.0.0-20200726213358-ec3860544ac0 // indirect
+)
